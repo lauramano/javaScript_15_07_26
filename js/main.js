3639c673ffceae3ee282b9b8479b12346666d558
laurapras <83669900+laurapras@users.noreply.github.com>
2022-07-26 14:17:03 +0300

    didziausia

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
 'use strict';
 console.log('labas');
+
+    const numbers=[1,2,3,5.3,8,-8,-5.5,-6,-16,-5,-9,4.2,5,12,-6,45];
+    const numberMax = numbers.reduce((maxSk, sk) =>Math.max(maxSk, sk), numbers[0]);
+    console.log('Didziausias skaicius', numberMax);
